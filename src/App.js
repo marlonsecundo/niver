e6a921f237b3395045f223a49e3ae04beda932ff
Marlon Secundo <marlon_secundo@outlook.com>
2022-08-23 12:19:01 -0300

    wip: final code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./animations.css";
 
 import backImg from "./images/background.png";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import mrLogo from "./images/mrrobot-logo.png";
 
 import confetti from "canvas-confetti";
 
+import elliot from "./images/elliot.png";
+
+import cakeday from "./images/cakeday.png";
+
 import Mouse from "./mouse";
 
 function App() {
+  const [keys, setKeys] = useState(new Array(10).fill(""));
+
   const [homeVisible, setHomeVisible] = useState(false);
+
+  const index = useRef(0);
 
   const [day, setDay] = useState("");
   const [month, setMonth] = useState("");
@@ -19,7 +28,6 @@ function App() {
   const handleConfirm = (d, m) => {
     const nDay = Number(d);
     const nMonth = Number(m);
-    // startFireworks();
 
     if (nDay === 9 && nMonth === 5) {
       setHomeVisible(true);
@@ -28,7 +36,7 @@ function App() {
   };
 
   const startFireworks = () => {
-    var duration = 15 * 10000000;
+    var duration = 15 * 1000;
     var animationEnd = Date.now() + duration;
     var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
@@ -60,34 +68,66 @@ function App() {
     }, 250);
   };
 
+  useEffect(() => {}, []);
+
   useEffect(() => {
-    // startFireworks();
-  }, []);
+    if (homeVisible) {
+      console.log("fsf");
+      window.addEventListener("keydown", (ev) => {
+        if (index.current >= keys.length) {
+          index.current = 0;
+        }
+        keys[index.current] = ev.key;
+
+        setKeys(keys.map((v) => v));
+
+        index.current += 1;
+      });
+    }
+  }, [homeVisible]);
+
+  useEffect(() => {
+    console.log(keys);
+    if (keys === ["w", "w", "s", "s", "a", "d", "a", "d", "b", "a"]) {
+      console.log("foi");
+    }
+  }, [keys]);
+
+  const squares = new Array(10).fill("").map((v, i) => {
+    return (
+      <li
+        key={i}
+        style={{
+          "--animColor": homeVisible ? "red" : "gray",
+        }}
+      ></li>
+    );
+  });
 
   return (
     <div className="App">
       <div className="area">
-        <ul className="circles">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
+        <ul className="circles">{squares}</ul>
       </div>
-      <Mouse></Mouse>
+      <Mouse happyVisible={homeVisible}></Mouse>
 
       {!homeVisible && (
         <section className="lockscreen">
           <p className="areaText">AREA PROTEGIDA...</p>
           <div className="content">
             <img src={mrLogo}></img>
-            <h2 className="digiteText">Data Chave</h2>
+            <h2 className="digiteText">
+              <span style={{ "--i": 1 }}>D</span>
+              <span style={{ "--i": 2 }}>A</span>
+              <span style={{ "--i": 3 }}>T</span>
+              <span style={{ "--i": 4 }}>A</span>
+              <span>_</span>
+              <span style={{ "--i": 5 }}>C</span>
+              <span style={{ "--i": 6 }}>H</span>
+              <span style={{ "--i": 7 }}>A</span>
+              <span style={{ "--i": 8 }}>V</span>
+              <span style={{ "--i": 9 }}>E</span>
+            </h2>
 
             <div className="inputs">
               <div className="column align-itens-center">
@@ -135,11 +175,40 @@ function App() {
 
       {homeVisible && (
         <section className={"home"}>
-          <h1 className="happyMsg">
-            Parabens
-            <br></br>
-            Julia
-          </h1>
+          <div className="column">
+            <h1 className="happyMsg">
+              Parabeeens
+              <br></br>
+              Juliaaaa
+            </h1>
+            <p className="happyText">
+              Ahhhhhhh, my frieeeendd parabenssss kkkkkk, venho por este site
+              trazer de maneira bem formal meu parabpens e felicidades por esta
+              incrivel pessoa que tu es. Pq fazer esse site todo e dedicar meu
+              tempo pra comemorar? pq sim e pq eu quis, eh isso.
+              <br></br>
+              <br></br>
+              (Se achou que eu ia elogiar, haha)
+              <br></br>
+              <br></br>
+              Feliz niver, eu quero bolo, e vc ja ta me devendo o groot....
+              (vale lembrar)
+              <br></br>
+              S2
+              <br></br>
+              <br></br>
+              <br></br>
+              Ahhh tem um cod no site, vamos ver se vc descobre, e não são os
+              confetes do mouse... (DICA: codigo universal dos jogos)
+            </p>
+
+            <p className="bottomText">
+              Ahhh e como levou um tempo pra fazer, quero um PIX de 10 reais.
+            </p>
+          </div>
+
+          <img className="elliotImage" src={elliot}></img>
+          <img className="cakedayImage" src={cakeday}></img>
         </section>
       )}
     </div>
--- src/mouse.js
+++ src/mouse.js
@@ -1,16 +1,43 @@
 import React, { useEffect, useState } from "react";
 import cursor from "./images/cursor.png";
+import confetti from "canvas-confetti";
 
-function Mouse() {
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function Mouse({ happyVisible }) {
   const [pos, setPos] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    window.addEventListener("mousemove", (ev) => {
-      console.log(ev.clientX);
-
+    const handleMouseMove = (ev) => {
       setPos({ x: ev.clientX, y: ev.clientY });
-    });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
   }, []);
+
+  useEffect(() => {
+    const handleMouseClick = (ev) => {
+      if (happyVisible) {
+        confetti({
+          angle: randomInRange(0, 360),
+          spread: randomInRange(50, 70),
+          particleCount: randomInRange(50, 100),
+          origin: { y: ev.y / window.innerHeight, x: ev.x / window.innerWidth },
+        });
+      }
+    };
+
+    window.addEventListener("mousedown", handleMouseClick);
+    return () => {
+      window.removeEventListener("mousedown", handleMouseClick);
+    };
+  }, [happyVisible]);
 
   return (
     <img
